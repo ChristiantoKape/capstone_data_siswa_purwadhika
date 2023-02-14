a2f71fb7d6a46fe8b8e9c79df4a290c54ab9998d
ChristiantoKape <christiantokape@gmail.com>
2023-02-14 15:50:18 +0700

    view_and_create_datas

--- datasiswa.py
+++ datasiswa.py
@@ -11,26 +11,91 @@ def menampilkanDataSiwa() :
 
     for i in range(len(dataSiswa)) :
         print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
-  
+
   def menampilkanSiswaTertentu():
-    print('--- Daftar Siswa ---\n')
-    print('Nisn\t| Nama  \t\t\t\t| Alamat\t\t| Tanggal Lahir')
-
-    if (len(dataSiswa) == 0):
-      print('\t\tTidak Ada Data Siswa!')
-    else: 
-      nisnSiswa = int(input('Masukkan Nisn Siswa: '))
-
-    #print data sesuai index yang dicari
-
+    inputData = input('Masukkan Nisn/Nama Siswa: ')
+    if(inputData.isdigit()):
+      for i in range(len(dataSiswa)) :
+        if(dataSiswa[i]['Nisn'] == inputData):
+          print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
+          break
+        else:
+          print('Data Siswa Tidak Ditemukan!')
+    else:
+      for i in range(len(dataSiswa)) :
+        if(dataSiswa[i]['Nama'] == inputData):
+          print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
+          break
+        else:
+          print('Data Siswa Tidak Ditemukan!')
+  
   while True:
-    pilihanData = input('\n---- Record Data Siswa ----\n\nList Menu: \n1. Menampilkan Raport Semua Siswa \n2. Menampilkan Raport Siswa Tertentu \n3. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-3]: ')
+    pilihanData = input('\n---- Record Data Siswa ----\n\nList Menu: \n1. Menampilkan Data Semua Siswa \n2. Menampilkan Data Siswa Berdasarkan Nama / Nisn \n3. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-3]: ')
 
     if(pilihanData == '1'):
       menampilkanSemuaSiswa()
     elif(pilihanData == '2'):
       menampilkanSiswaTertentu()
     elif(pilihanData == '3'):
+      break
+    else:
+      print('*****Pilihan yang anda masukkan salah!*****')
+
+def menambahDataSiswa():
+  def addSiswaRecord():
+    nisnSiswa = input('Masukkan Nisn Siswa: ')
+
+    # jika nisn tidak angka maka akan diulang
+    while(not nisnSiswa.isdigit()):
+      print('NISN Harus Angka! Masukkan NISN Ulang!')
+      nisnSiswa = input('Masukkan Nisn Siswa: ')
+
+    #validasi nisn agar tidak ada yang sama
+    for i in range(len(dataSiswa)):
+      if(dataSiswa[i]['Nisn'] == nisnSiswa):
+        print('*****Nisn Siswa Sudah Terdaftar!*****')
+        return
+
+    namaSiswa = input('Masukkan Nama Siswa: ')
+    alamatSiswa = input('Masukkan Alamat Siswa: ')
+    tanggalLahirSiswa = input('Masukkan Tanggal Lahir Siswa: ')
+
+    # split data siswa untuk capitalize sebelum append ke directory
+    namaSiswa = namaSiswa.split()
+    for i in range(len(namaSiswa)):
+      namaSiswa[i] = namaSiswa[i].capitalize()
+    namaSiswa = ' '.join(namaSiswa)
+
+    # split alamat untuk capitalize sebelum append ke directory
+    alamatSiswa = alamatSiswa.split()
+    for i in range(len(alamatSiswa)):
+      alamatSiswa[i] = alamatSiswa[i].capitalize()
+    alamatSiswa = ' '.join(alamatSiswa)
+
+    # check apakah ingin menambahkan data siswa
+    checker = input('Apakah anda ingin menambahkan data siswa? [Y/N]: ')
+
+    #jika memilih selain N dan Y maka akan diulang
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda ingin menambahkan data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+
+    dataSiswa.append({
+      'Nisn': nisnSiswa,
+      'Nama': namaSiswa,
+      'Alamat': alamatSiswa,
+      'Tanggal Lahir': tanggalLahirSiswa
+    })
+
+    print('---Data Siswa Berhasil Ditambahkan!---')
+
+  while True:
+    pilihanData = input('\n---- Menambahkan Data Siswa ----\n\nList Menu: \n1. Menambah Data Siswa \n2. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-2]: ')
+
+    if(pilihanData == '1'):
+      addSiswaRecord()
+    elif(pilihanData == '2'):
       break
     else:
       print('*****Pilihan yang anda masukkan salah!*****')
