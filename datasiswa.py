f43f4f2ce89476b97f2414dabad5cd0be08251dc
ChristiantoKape <christiantokape@gmail.com>
2023-02-19 17:16:40 +0700

    search_siswa_and_notification

--- datasiswa.py
+++ datasiswa.py
@@ -1,52 +1,33 @@
 dataSiswa = [
-]
-
-def hasil(i):
-    print('Nisn\t| Nama  \t\t\t\t| Alamat\t\t| Tanggal Lahir')
-    print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
+]  
 
 def menampilkanSemuaSiswa():
   print('--- Daftar Siswa ---\n')
   if (len(dataSiswa) == 0):
-    print('\t\tTidak Ada Data Siswa!')
+    print('Tidak Ada Data Siswa!')
 
   for i in range(len(dataSiswa)) :
-    hasil(i)
+    print('Nisn: {}, Nama: {}, Kota: {}, Jenis Kelamin: {}, Tanggal Lahir: {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Kota'],dataSiswa[i]['Jenis Kelamin'],dataSiswa[i]['Tanggal Lahir']))
 
-def menampilkanSiswaTertentu():
-  inputData = input('Masukkan Nisn/Nama Siswa: ')
-  if(inputData.isdigit()):
-    for i in range(len(dataSiswa)) :
-      if(dataSiswa[i]['Nisn'] == inputData):
-        print('--- Data Siswa ---\n')
-        hasil(i)
-        break
-      else:
-        print('Data Siswa Tidak Ditemukan!')
-  else:
-    for i in range(len(dataSiswa)) :
-      if(dataSiswa[i]['Nama'] == inputData):
-        print('--- Data Siswa ---\n')
-        hasil(i)
-        break
-      else:
-        print('Data Siswa Tidak Ditemukan!')
+def mencariDataSiswa():
+  # input data siswa
+  inputData = input('Masukkan Nisn Siswa: ')
+  print(inputData.capitalize())
 
-def menampilkanDataSiwa() :  
-  while True:
-    pilihanData = input('\n---- Record Data Siswa ----\n\nList Menu: \n1. Menampilkan Data Semua Siswa \n2. Menampilkan Data Siswa Berdasarkan Nama / Nisn \n3. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-3]: ')
-
-    if(pilihanData == '1'):
-      menampilkanSemuaSiswa()
-    elif(pilihanData == '2'):
-      menampilkanSiswaTertentu()
-    elif(pilihanData == '3'):
+  # check nisn angka atau bukan
+  result = None
+  for sub in dataSiswa:
+    if sub['Nisn'] == inputData.capitalize():
+      result = sub
       break
-    else:
-      print('*****Pilihan yang anda masukkan salah!*****')
 
-def menambahDataSiswa():
-  def addSiswaRecord():
+  print("-- Data Siswa yang Dicari --\n")
+  if(result):
+    print('Nisn: {}, Nama: {}, Kota: {}, Jenis Kelamin: {}, Tanggal Lahir: {}'.format(result['Nisn'],result['Nama'],result['Kota'],result['Jenis Kelamin'],result['Tanggal Lahir']))
+  else:
+    print('Tidak Ada Data Siswa!')
+
+def addSiswaRecord():
     nisnSiswa = input('Masukkan Nisn Siswa: ')
 
     # jika nisn tidak angka maka akan diulang
@@ -61,7 +42,8 @@ def menambahDataSiswa():
         return
 
     namaSiswa = input('Masukkan Nama Siswa: ')
-    alamatSiswa = input('Masukkan Alamat Siswa: ')
+    alamatSiswa = input('Masukkan Kota Siswa: ')
+    jenisKelamin = input('Masukkan Jenis Kelamin Siswa: ')
     tanggalLahirSiswa = input('Masukkan Tanggal Lahir Siswa: ')
 
     # split data siswa untuk capitalize sebelum append ke directory
@@ -75,6 +57,12 @@ def menambahDataSiswa():
     for i in range(len(alamatSiswa)):
       alamatSiswa[i] = alamatSiswa[i].capitalize()
     alamatSiswa = ' '.join(alamatSiswa)
+
+    # split jenis kelamin untuk capitalize sebelum append ke directory
+    jenisKelamin = jenisKelamin.split()
+    for i in range(len(jenisKelamin)):
+      jenisKelamin[i] = jenisKelamin[i].capitalize()
+    jenisKelamin = '-'.join(jenisKelamin)
 
     # check apakah ingin menambahkan data siswa
     checker = input('Apakah anda ingin menambahkan data siswa? [Y/N]: ')
@@ -88,12 +76,143 @@ def menambahDataSiswa():
     dataSiswa.append({
       'Nisn': nisnSiswa,
       'Nama': namaSiswa,
-      'Alamat': alamatSiswa,
+      'Kota': alamatSiswa,
+      'Jenis Kelamin': jenisKelamin,
       'Tanggal Lahir': tanggalLahirSiswa
     })
 
     print('---Data Siswa Berhasil Ditambahkan!---')
 
+def changeDataSiswa():
+  menampilkanSemuaSiswa()
+
+  if(len(dataSiswa) == 0):
+    return
+
+  inputData = input('Masukkan Nisn Siswa yang Ingin di Ubah: ')
+
+  res = None
+  for sub in dataSiswa:
+      if sub['Nisn'] == inputData:
+          res = sub
+          break
+
+  if(res == None):
+    print('Tidak Ada Data Siswa!')
+    return
+  else:
+    print("-- Data Siswa yang Dicari --\n")
+    print('Nisn: {}, Nama: {}, Kota: {}, Jenis Kelamin: {}, Tanggal Lahir: {}'.format(res['Nisn'],res['Nama'],res['Kota'],res['Jenis Kelamin'],res['Tanggal Lahir']))
+    
+  checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+
+  while(checker.upper() != 'Y' and checker.upper() != 'N'):
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+    if(checker == 'N'):
+      return
+
+  inputData = input('Masukkan Kolom yang Ingin di Ubah: ')
+
+  inputData = inputData.split()
+  for i in range(len(inputData)):
+    inputData[i] = inputData[i].capitalize()
+  inputData = ' '.join(inputData)
+
+  if(inputData == 'Nisn'):
+    nisnSiswa = input('Masukkan Nisn Siswa: ')
+
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+    
+    res['Nisn'] = nisnSiswa
+    print('---Data NISN Berhasil Diubah!---')
+
+  elif(inputData == 'Nama'):
+    namaSiswa = input('Masukkan Nama Siswa: ')
+
+    namaSiswa = namaSiswa.split()
+    for i in range(len(namaSiswa)):
+      namaSiswa[i] = namaSiswa[i].capitalize()
+    namaSiswa = ' '.join(namaSiswa)
+
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+
+    res['Nama'] = namaSiswa
+    print('---Data NISN Berhasil Diubah!---')
+
+  elif(inputData == 'Kota'):
+    alamatSiswa = input('Masukkan Kota Siswa: ')
+
+    alamatSiswa = alamatSiswa.split()
+    for i in range(len(alamatSiswa)):
+      alamatSiswa[i] = alamatSiswa[i].capitalize()
+    alamatSiswa = ' '.join(alamatSiswa)
+
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+
+    res['Kota'] = alamatSiswa
+    print('---Data NISN Berhasil Diubah!---')
+
+  elif(inputData == 'Jenis Kelamin'):
+    jenisKelamin = input('Masukkan Jenis Kelamin Siswa: ')
+
+    jenisKelamin = jenisKelamin.split()
+    for i in range(len(jenisKelamin)):
+      jenisKelamin[i] = jenisKelamin[i].capitalize()
+    jenisKelamin = '-'.join(jenisKelamin)
+
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+
+    res['Jenis Kelamin'] = jenisKelamin
+    print('---Data NISN Berhasil Diubah!---')
+
+  elif(inputData == 'Tanggal Lahir'):
+    tanggalLahirSiswa = input('Masukkan Tanggal Lahir Siswa: ')
+
+    tanggalLahirSiswa = tanggalLahirSiswa.split()
+    for i in range(len(tanggalLahirSiswa)):
+      tanggalLahirSiswa[i] = tanggalLahirSiswa[i]
+    tanggalLahirSiswa = ' '.join(tanggalLahirSiswa)
+
+    checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+    while(checker.upper() != 'Y' and checker.upper() != 'N'):
+      checker = input('Apakah anda yakin ingin mengubah data siswa? [Y/N]: ')
+      if(checker == 'N'):
+        return
+    
+    res['Tanggal Lahir'] = tanggalLahirSiswa
+    print('---Data NISN Berhasil Diubah!---')
+
+def menampilkanDataSiwa() :
+  while True:
+    pilihanData = input('\n---- Record Data Siswa ----\n\nList Menu: \n1. Menampilkan Data Semua Siswa \n2. Mencari Data Sesuai Nisn Siswa \n3. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-2]: ')
+
+    if(pilihanData == '1'):
+      menampilkanSemuaSiswa()
+    elif(pilihanData == '2'):
+      mencariDataSiswa()
+    elif(pilihanData == '3'):
+      break
+    else:
+      print('*****Pilihan yang anda masukkan salah!*****')
+
+def menambahDataSiswa():
   while True:
     pilihanData = input('\n---- Menambahkan Data Siswa ----\n\nList Menu: \n1. Menambah Data Siswa \n2. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-2]: ')
 
@@ -104,93 +223,12 @@ def menambahDataSiswa():
     else:
       print('*****Pilihan yang anda masukkan salah!*****')
 
-def mengubahDataSiswa():
-  def changeSiswaRecord():
-    # input nisn siswa yang akan diubah
-    inputData = input('Masukkan Nisn/Nama Siswa: ')
-
-    # jika inputan berupa angka maka akan dicari berdasarkan nisn jika tidak ditemukan maka akan diulang inputan
-    if(inputData.isdigit()):
-      for i in range(len(dataSiswa)) :
-        if(dataSiswa[i]['Nisn'] == inputData):
-          print('--- Data Siswa ---\n')
-          print('Nisn\t| Nama  \t\t\t\t| Alamat\t\t| Tanggal Lahir')
-          print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
-          break
-        else:
-          print('Data Siswa Tidak Ditemukan!')
-          return
-    else:
-      for i in range(len(dataSiswa)) :
-        if(dataSiswa[i]['Nama'] == inputData):
-          print('--- Data Siswa ---\n')
-          print('Nisn\t| Nama  \t\t\t\t| Alamat\t\t| Tanggal Lahir')
-          print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
-          break
-        else:
-          print('Data Siswa Tidak Ditemukan!')
-          return
-
-    # checker untuk mengecek apakah ingin mengubah data siswa
-    checker = input('Apakah anda ingin mengubah data siswa? [Y/N]: ')
-
-    #jika memilih selain N dan Y maka akan diulang
-    while(checker.upper() != 'Y' and checker.upper() != 'N'):
-      checker = input('Apakah anda ingin mengubah data siswa? [Y/N]: ')
-      if(checker == 'N'):
-        return
-
-    # input data baru
-    nisnSiswa = input('Masukkan Nisn Siswa: ')
-    namaSiswa = input('Masukkan Nama Siswa: ')
-    alamatSiswa = input('Masukkan Alamat Siswa: ')
-    tanggalLahirSiswa = input('Masukkan Tanggal Lahir Siswa: ')
-
-    # split data siswa untuk capitalize sebelum append ke directory
-    namaSiswa = namaSiswa.split()
-    for i in range(len(namaSiswa)):
-      namaSiswa[i] = namaSiswa[i].capitalize()
-    namaSiswa = ' '.join(namaSiswa)
-
-    # split alamat untuk capitalize sebelum append ke directory
-    alamatSiswa = alamatSiswa.split()
-    for i in range(len(alamatSiswa)):
-      alamatSiswa[i] = alamatSiswa[i].capitalize()
-    alamatSiswa = ' '.join(alamatSiswa)
-
-    # check apakah ingin mengubah data siswa
-    checker = input('Apakah Data Sudah Benar? [Y/N]: ')
-
-    #jika memilih selain N dan Y maka akan diulang
-    while(checker.upper() != 'Y' and checker.upper() != 'N'):
-      checker = input('Apakah Data Sudah Benar? [Y/N]: ')
-      if(checker == 'N'):
-        return
-
-    # jika input kosong menggunakan data lama siswa
-    if(nisnSiswa == ''):
-      nisnSiswa = dataSiswa[i]['Nisn']
-    if(namaSiswa == ''):
-      namaSiswa = dataSiswa[i]['Nama']
-    if(alamatSiswa == ''):
-      alamatSiswa = dataSiswa[i]['Alamat']
-    if(tanggalLahirSiswa == ''):
-      tanggalLahirSiswa = dataSiswa[i]['Tanggal Lahir']
-
-    # mengubah data siswa pada directory
-    dataSiswa[i]['Nisn'] = nisnSiswa
-    dataSiswa[i]['Nama'] = namaSiswa
-    dataSiswa[i]['Alamat'] = alamatSiswa
-    dataSiswa[i]['Tanggal Lahir'] = tanggalLahirSiswa
-
-    print('---Data Siswa Berhasil Diubah!---')
-
-    
+def mengubahDataSiswa():        
   while True:
     pilihanData = input('\n---- Mengubah Data Siswa ----\n\nList Menu: \n1. Mengubah Data Siswa \n2. Kembali ke Menu Utama \nSilahkan Pilih Sub-Menu [1-2]: ')
 
     if(pilihanData == '1'):
-      changeSiswaRecord()
+      changeDataSiswa()
     elif(pilihanData == '2'):
       break
     else:
@@ -199,37 +237,34 @@ def mengubahDataSiswa():
 def menghapusDataSiswa():
   menampilkanSemuaSiswa()
 
-  # input nisn siswa yang akan dihapus
-  inputData = input('Masukkan Nisn/Nama Siswa yang Ingin di Hapus: ')
+  if(len(dataSiswa) == 0):
+    return
 
-  # check apakah data siswa ada
+  inputData = input('Masukkan Nisn Siswa yang Ingin di Hapus: ')
+
   if(inputData.isdigit()):
-    for i in range(len(dataSiswa)) :
+    i = 0
+    while i < len(dataSiswa):
       if(dataSiswa[i]['Nisn'] == inputData):
         print('--- Data Siswa ---\n')
-        print('Nisn\t| Nama  \t\t\t\t| Alamat\t\t| Tanggal Lahir')
-        print('{}\t| {} \t| {}\t| {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Alamat'],dataSiswa[i]['Tanggal Lahir']))
+        print('Nisn: {}, Nama: {}, Kota: {}, Jenis Kelamin: {}, Tanggal Lahir: {}'.format(dataSiswa[i]['Nisn'],dataSiswa[i]['Nama'],dataSiswa[i]['Kota'],dataSiswa[i]['Jenis Kelamin'],dataSiswa[i]['Tanggal Lahir']))
         break
       else:
-        # jika data siswa tidak ditemukan maka mengulang input
         print('Data Siswa Tidak Ditemukan!')
-        inputData = input('Masukkan Nisn/Nama siswa Dengan Benar!: ')
+        inputData = input('Masukkan Nisn siswa Dengan Benar!: ')
 
-  # checker apakah yakin menghapus data siswa
   checker = input('Apakah anda yakin ingin menghapus data siswa? [Y/N]: ')
 
-  #jika memilih selain N dan Y maka akan diulang
   while(checker.upper() != 'Y' and checker.upper() != 'N'):
     checker = input('Apakah anda yakin ingin menghapus data siswa? [Y/N]: ')
     if(checker == 'N'):
       return
 
-  # menghapus data siswa
   del dataSiswa[i]
   print('---Data Siswa Berhasil Dihapus!---')
 
 while True:
-  pilihanMenu = input('\n---- Data Raport Siswa ----\n\nList Menu: \n1. Menampilkan Data Siswa \n2. Menambah Data Siswa \n3. Mengubah Data Siswa \n4. Menghapus Data Siswa \n5. Exit Program \nSilahkan Pilih Menu [1-5]: ')
+  pilihanMenu = input('\n---- Record Data Siswa ----\n\nList Menu: \n1. Menampilkan Data Siswa \n2. Menambah Data Siswa \n3. Mengubah Data Siswa \n4. Menghapus Data Siswa \n5. Exit Program \nSilahkan Pilih Menu [1-5]: ')
 
   if(pilihanMenu == '1'):
     menampilkanDataSiwa()
